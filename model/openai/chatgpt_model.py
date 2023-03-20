00b11e470f32c3ea21b3f7df79d3a2860e0beb5d
liusishan <33129823+liusishan@users.noreply.github.com>
2023-03-20 23:48:54 +0800

    Update chatgpt_model.py

--- model/openai/chatgpt_model.py
+++ model/openai/chatgpt_model.py
@@ -13,6 +13,8 @@ user_session = dict()
 class ChatGPTModel(Model):
     def __init__(self):
         openai.api_key = model_conf(const.OPEN_AI).get('api_key')
+        if model_conf(const.OPEN_AI).get('api_base'):
+            openai.api_base = model_conf(const.OPEN_AI).get('api_base')
         proxy = model_conf(const.OPEN_AI).get('proxy')
         if proxy:
             openai.proxy = proxy
